2a268191225e2207a8f67ed17a975787c64077f6
Yuri Santana <yuri.luana2016@gmail.com>
2023-10-26 18:54:05 -0300

    correcao playback

--- src/app/progress-spinner/progress-spinner.component.ts
+++ src/app/progress-spinner/progress-spinner.component.ts
@@ -9,13 +9,13 @@ import { concat, interval, map, take, takeWhile, tap } from 'rxjs';
 })
 export class ProgressSpinnerComponent {
   public loadingPercent = 0;
-  public loadingPercentBuffer = 0;
   public queryValue = 0;
   public queryMode: ProgressBarMode = 'query';
   public queryBuffer = 0;
 
   ngOnInit(): void {
-    this.loadingProgress(500, 95).subscribe((i) => (this.loadingPercent = i));
+    this.loadingProgress(500, 100).subscribe((i) => (this.loadingPercent = i));
+    this.loadingProgress(375, 100).subscribe((i) => (this.queryBuffer = i));
 
     concat(
       // observable 1
@@ -26,34 +26,13 @@ export class ProgressSpinnerComponent {
         })
       ),
       // observable 2
-      // this.loadingProgress(500, 100),
-      // // observable 3
-      // interval(500).pipe(
-      //   take(1),
-      //   tap((_) => {
-      //     this.queryMode = 'determinate';
-      //     console.log('foi 3');
-      //   })
-      // ),
-      // // observable 4
-      this.loadingProgress(500, 10)
+      this.loadingProgress(500, 100)
     ).subscribe((i) => (this.queryValue = i));
-    this.loadingBuffer(100, 100).subscribe((i) => (this.queryBuffer = i));
   }
 
   loadingProgress(speed: number, takeUntil: number) {
     return interval(speed).pipe(
       map((i) => i * 5),
-      takeWhile((i) => i <= takeUntil)
-    );
-  }
-
-  loadingBuffer(speed: number, takeUntil: number) {
-    this.loadingProgress(speed, 100).subscribe(
-      (i) => (this.loadingPercentBuffer = i)
-    );
-    return interval(speed).pipe(
-      tap((i) => (this.queryBuffer = i * 50)),
       takeWhile((i) => i <= takeUntil)
     );
   }
